fa0e7c72a3bc45d0b0dc9b37dcce465f78f6fa7e
Arthur Pastel <arthur.pastel@gmail.com>
2023-01-17 16:55:01 +0100

    feat: add release script

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Usage: ./scripts/release.sh <major|minor|patch>
+set -ex
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ./release.sh <major|minor|patch>"
+  exit 1
+fi
+
+# Fail if there are any unstaged changes left
+git diff --exit-code
+
+pnpm lerna version $1 -y
+pnpm moon run :build
+pnpm lerna publish from-package -y
+NEW_VERSION=$(pnpm lerna list --json | jq -r '.[] | select(.name == "@codspeed/core") | .version')
+gh release create v$NEW_VERSION --title "v$NEW_VERSION" --generate-notes -d
