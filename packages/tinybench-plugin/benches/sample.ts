448b5ca0fc95c2c899f07f78ac0e0c558016909f
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-11-10 14:58:05 +0100

    chore: add some benches to plugins

--- packages/tinybench-plugin/benches/sample.ts
+++ packages/tinybench-plugin/benches/sample.ts
@@ -1,7 +1,17 @@
 import { Bench } from "tinybench";
 import { withCodSpeed } from "..";
+import parsePr from "./parsePr";
+
+const LONG_BODY =
+  new Array(1_000)
+    .fill(
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, earum. Atque architecto vero veniam est tempora fugiat sint quo praesentium quia. Autem, veritatis omnis beatae iste delectus recusandae animi non."
+    )
+    .join("\n") + "fixes #123";
 
 const bench = withCodSpeed(new Bench({ time: 100 }));
+
+console.log(`PROCESS PID: ${process.pid} in ${__filename}`);
 
 bench
   .add("switch 1", () => {
@@ -17,6 +27,12 @@ bench
     a = b + a;
     b = a - b;
     a = b - a;
+  })
+  .add("short body", () => {
+    parsePr({ body: "fixes #123", title: "test", number: 124 });
+  })
+  .add("long body", () => {
+    parsePr({ body: LONG_BODY, title: "test", number: 124 });
   });
 
 bench.run().then(() => {
