4319e3541ad1d37a2826b34f4404e19595baf7ac
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2023-12-07 17:15:51 +0100

    feat(core): remove handling of mongo env var

--- packages/core/src/mongoMeasurement.ts
+++ packages/core/src/mongoMeasurement.ts
@@ -5,17 +5,6 @@ export class MongoMeasurement {
 
   constructor() {
     const serverUrl = process.env.CODSPEED_MONGO_INSTR_SERVER_ADDRESS;
-    // TODO
-    const mongoUriEnvName = process.env.CODSPEED_MONGO_INSTR_URI_ENV_NAME;
-    if (mongoUriEnvName === undefined) {
-      throw new Error("CODSPEED_MONGO_INSTR_URI_ENV_NAME is not defined");
-    }
-    const mongoUri = process.env[mongoUriEnvName];
-    if (mongoUri === undefined) {
-      throw new Error(`Environment variable ${mongoUriEnvName} is not defined`);
-    }
-    process.env[mongoUriEnvName] =
-      "mongodb://localhost:27018?directConnection=true";
 
     if (serverUrl !== undefined) {
       this.tracerClient = new MongoTracer({
