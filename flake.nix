cbb7a256e4020fa4c86714becb6531b6c9a41453
Guillaume Lagrange <guillaume@glagrange.eu>
2025-07-18 10:36:47 +0200

    feat: add flake for native build python dependency

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "CodSpeed Node development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        commonBuildInputs = with pkgs; [
+          # Needed for node-gyp
+          (python314.withPackages (
+            ps: with ps; [
+              setuptools
+            ]
+          ))
+          valgrind # TODO: Remove this in favor of codspeed's valgrind
+        ];
+
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = commonBuildInputs;
+            shellHook = ''
+              echo "CodSpeed Node development environment"
+            '';
+          };
+
+          lsp = pkgs.mkShell {
+            buildInputs =
+              with pkgs;
+              [
+                typescript-language-server
+              ]
+              ++ commonBuildInputs;
+            shellHook = ''
+              echo "CodSpeed Node development environment with LSP"
+            '';
+          };
+        };
+      }
+    );
+}
