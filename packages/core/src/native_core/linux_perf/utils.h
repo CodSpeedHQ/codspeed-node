d5e8d2433609aac574da1055a4b31796912eae95
Guillaume Lagrange <guillaume@glagrange.eu>
2025-08-28 15:46:17 +0200

    feat: fix compilation warning about deprecated native API
    Bumping prebuildify to include up to date node headers

--- packages/core/src/native_core/linux_perf/utils.h
+++ packages/core/src/native_core/linux_perf/utils.h
@@ -5,9 +5,9 @@
 
 static inline std::string
 v8LocalStringToString(v8::Local<v8::String> v8String) {
-  std::string buffer(v8String->Utf8Length(v8::Isolate::GetCurrent()) + 1, 0);
-  v8String->WriteUtf8(v8::Isolate::GetCurrent(), &buffer[0],
-                      v8String->Utf8Length(v8::Isolate::GetCurrent()) + 1);
+  std::string buffer(v8String->Utf8LengthV2(v8::Isolate::GetCurrent()) + 1, 0);
+  v8String->WriteUtf8V2(v8::Isolate::GetCurrent(), &buffer[0],
+                      v8String->Utf8LengthV2(v8::Isolate::GetCurrent()) + 1);
   // Sanitize name, removing unwanted \0 resulted from WriteUtf8
   return std::string(buffer.c_str());
 }
