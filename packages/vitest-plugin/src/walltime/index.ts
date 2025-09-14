44a2b8dcd8c0b49602e0514d6ec7d27b682380b3
Guillaume Lagrange <guillaume@glagrange.eu>
2025-09-14 18:10:03 +0200

    feat(vitest-plugin): add perf profiling for vitest plugin

--- packages/vitest-plugin/src/walltime/index.ts
+++ packages/vitest-plugin/src/walltime/index.ts
@@ -1,5 +1,14 @@
-import { setupCore, writeWalltimeResults } from "@codspeed/core";
-import { type RunnerTestSuite } from "vitest";
+import {
+  InstrumentHooks,
+  setupCore,
+  writeWalltimeResults,
+} from "@codspeed/core";
+import { Fn } from "tinybench";
+import {
+  RunnerTaskEventPack,
+  RunnerTaskResultPack,
+  type RunnerTestSuite,
+} from "vitest";
 import { NodeBenchmarkRunner } from "vitest/runners";
 import { patchRootSuiteWithFullFilePath } from "../common";
 import { extractBenchmarkResults } from "./utils";
@@ -10,10 +19,13 @@ import { extractBenchmarkResults } from "./utils";
  */
 export class WalltimeRunner extends NodeBenchmarkRunner {
   private isTinybenchHookedWithCodspeed = false;
-  private benchmarkUris = new Map<string, string>();
+  private suiteUris = new Map<string, string>();
+  /// Suite ID of the currently running suite, to allow constructing the URI in the context of tinybench tasks
+  private currentSuiteId: string | null = null;
 
   async runSuite(suite: RunnerTestSuite): Promise<void> {
     patchRootSuiteWithFullFilePath(suite);
+    this.populateBenchmarkUris(suite);
 
     setupCore();
 
@@ -31,6 +43,72 @@ export class WalltimeRunner extends NodeBenchmarkRunner {
         `[CodSpeed] No benchmark results found after suite execution`
       );
     }
+  }
+
+  private populateBenchmarkUris(suite: RunnerTestSuite, parentPath = ""): void {
+    const currentPath =
+      parentPath !== "" ? `${parentPath}::${suite.name}` : suite.name;
+
+    for (const task of suite.tasks) {
+      if (task.type === "suite") {
+        this.suiteUris.set(task.id, `${currentPath}::${task.name}`);
+        this.populateBenchmarkUris(task, currentPath);
+      }
+    }
+  }
+
+  async importTinybench(): Promise<typeof import("tinybench")> {
+    const tinybench = await super.importTinybench();
+
+    if (this.isTinybenchHookedWithCodspeed) {
+      return tinybench;
+    }
+    this.isTinybenchHookedWithCodspeed = true;
+
+    const originalRun = tinybench.Task.prototype.run;
+
+    const getSuiteUri = (): string => {
+      if (this.currentSuiteId === null) {
+        throw new Error("currentSuiteId is null - something went wrong");
+      }
+      return this.suiteUris.get(this.currentSuiteId) || "";
+    };
+
+    tinybench.Task.prototype.run = async function () {
+      const { fn } = this as { fn: Fn };
+      const suiteUri = getSuiteUri();
+
+      function __codspeed_root_frame__() {
+        return fn();
+      }
+      (this as { fn: Fn }).fn = __codspeed_root_frame__;
+
+      InstrumentHooks.startBenchmark();
+      await originalRun.call(this);
+      InstrumentHooks.stopBenchmark();
+
+      // Look up the URI by task name
+      const uri = `${suiteUri}::${this.name}`;
+      InstrumentHooks.setExecutedBenchmark(process.pid, uri);
+
+      return this;
+    };
+
+    return tinybench;
+  }
+
+  // Allow tinybench to retrieve the path to the currently running suite
+  async onTaskUpdate(
+    _: RunnerTaskResultPack[],
+    events: RunnerTaskEventPack[]
+  ): Promise<void> {
+    events.map((event) => {
+      const [id, eventName] = event;
+
+      if (eventName === "suite-prepare") {
+        this.currentSuiteId = id;
+      }
+    });
   }
 }
 
